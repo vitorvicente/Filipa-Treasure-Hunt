f4efebaab0d4eb23c09ef12474ae66fc47c674cf
Victor Vicente <vitor@bu.edu>
2022-07-31 15:43:50 -0700

    First Clue Finished

---
+++ src/assets/BaseData.js
@@ -0,0 +1,740 @@
+export const WORD_SEARCH_WORD = [
+  {
+    word: "fulbright",
+    status: false,
+    tip: "The Place where it began",
+    tipNumber: 1,
+    color: "#FF0000",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "f",
+        x: 0,
+        y: 0
+      },
+      {
+        letter: "u",
+        x: 1,
+        y: 1
+      },
+      {
+        letter: "l",
+        x: 2,
+        y: 2
+      },
+      {
+        letter: "b",
+        x: 3,
+        y: 3
+      },
+      {
+        letter: "r",
+        x: 4,
+        y: 4
+      },
+      {
+        letter: "i",
+        x: 5,
+        y: 5
+      },
+      {
+        letter: "g",
+        x: 6,
+        y: 6
+      },
+      {
+        letter: "h",
+        x: 7,
+        y: 7
+      },
+      {
+        letter: "t",
+        x: 8,
+        y: 8
+      }
+    ]
+  },
+  {
+    word: "helipad",
+    status: false,
+    tip: "Where we used to sit and watch the night lights",
+    tipNumber: 2,
+    color: "#FF8900",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "h",
+        x: 0,
+        y: 2
+      },
+      {
+        letter: "e",
+        x: 0,
+        y: 3
+      },
+      {
+        letter: "l",
+        x: 0,
+        y: 4
+      },
+      {
+        letter: "i",
+        x: 0,
+        y: 5
+      },
+      {
+        letter: "p",
+        x: 0,
+        y: 6
+      },
+      {
+        letter: "a",
+        x: 0,
+        y: 7
+      },
+      {
+        letter: "d",
+        x: 0,
+        y: 8
+      }
+    ]
+  },
+  {
+    word: "consistency",
+    status: false,
+    tip: "Your first great work",
+    tipNumber: 3,
+    color: "#00FFF3",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "c",
+        x: 11,
+        y: 0
+      },
+      {
+        letter: "o",
+        x: 10,
+        y: 0
+      },
+      {
+        letter: "n",
+        x: 9,
+        y: 0
+      },
+      {
+        letter: "s",
+        x: 8,
+        y: 0
+      },
+      {
+        letter: "i",
+        x: 7,
+        y: 0
+      },
+      {
+        letter: "s",
+        x: 6,
+        y: 0
+      },
+      {
+        letter: "t",
+        x: 5,
+        y: 0
+      },
+      {
+        letter: "e",
+        x: 4,
+        y: 0
+      },
+      {
+        letter: "n",
+        x: 3,
+        y: 0
+      },
+      {
+        letter: "c",
+        x: 2,
+        y: 0
+      },
+      {
+        letter: "y",
+        x: 1,
+        y: 0
+      }
+    ]
+  },
+  {
+    word: "west wing",
+    status: false,
+    tip: "The longest running companion",
+    tipNumber: 4,
+    color: "#00FFAC",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "w",
+        x: 1,
+        y: 8
+      },
+      {
+        letter: "e",
+        x: 2,
+        y: 7
+      },
+      {
+        letter: "s",
+        x: 3,
+        y: 6
+      },
+      {
+        letter: "t",
+        x: 4,
+        y: 5
+      },
+      {
+        letter: "w",
+        x: 5,
+        y: 4
+      },
+      {
+        letter: "i",
+        x: 6,
+        y: 3
+      },
+      {
+        letter: "n",
+        x: 7,
+        y: 2
+      },
+      {
+        letter: "g",
+        x: 8,
+        y: 1
+      }
+    ]
+  },
+  {
+    word: "kent",
+    status: false,
+    tip: "The Garden of England",
+    tipNumber: 5,
+    color: "#00FF2C",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "k",
+        x: 3,
+        y: 7
+      },
+      {
+        letter: "e",
+        x: 4,
+        y: 7
+      },
+      {
+        letter: "n",
+        x: 5,
+        y: 7
+      },
+      {
+        letter: "t",
+        x: 6,
+        y: 7
+      }
+    ]
+  },
+  {
+    word: "oxford",
+    status: false,
+    tip: "Our first trip",
+    tipNumber: 6,
+    color: "#FFE800",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "o",
+        x: 3,
+        y: 1
+      },
+      {
+        letter: "x",
+        x: 4,
+        y: 2
+      },
+      {
+        letter: "f",
+        x: 5,
+        y: 3
+      },
+      {
+        letter: "o",
+        x: 6,
+        y: 4
+      },
+      {
+        letter: "r",
+        x: 7,
+        y: 5
+      },
+      {
+        letter: "d",
+        x: 8,
+        y: 6
+      }
+    ]
+  },
+  {
+    word: "amazon",
+    status: false,
+    tip: "My Future Home",
+    tipNumber: 7,
+    color: "#007DFF",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "a",
+        x: 6,
+        y: 1
+      },
+      {
+        letter: "m",
+        x: 5,
+        y: 2
+      },
+      {
+        letter: "a",
+        x: 4,
+        y: 3
+      },
+      {
+        letter: "z",
+        x: 3,
+        y: 4
+      },
+      {
+        letter: "o",
+        x: 2,
+        y: 5
+      },
+      {
+        letter: "n",
+        x: 1,
+        y: 6
+      }
+    ]
+  },
+  {
+    word: "hamilton",
+    status: false,
+    tip: "And the world is gonna know your name...",
+    tipNumber: 8,
+    color: "#FF8989",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "h",
+        x: 11,
+        y: 8
+      },
+      {
+        letter: "a",
+        x: 11,
+        y: 7
+      },
+      {
+        letter: "m",
+        x: 11,
+        y: 6
+      },
+      {
+        letter: "i",
+        x: 11,
+        y: 5
+      },
+      {
+        letter: "l",
+        x: 11,
+        y: 4
+      },
+      {
+        letter: "t",
+        x: 11,
+        y: 3
+      },
+      {
+        letter: "o",
+        x: 11,
+        y: 2
+      },
+      {
+        letter: "n",
+        x: 11,
+        y: 1
+      }
+    ]
+  },
+  {
+    word: "boston",
+    status: false,
+    tip: "Our Latest stop",
+    tipNumber: 9,
+    color: "#F900FF",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "b",
+        x: 7,
+        y: 8
+      },
+      {
+        letter: "o",
+        x: 6,
+        y: 8
+      },
+      {
+        letter: "s",
+        x: 5,
+        y: 8
+      },
+      {
+        letter: "t",
+        x: 4,
+        y: 8
+      },
+      {
+        letter: "o",
+        x: 3,
+        y: 8
+      },
+      {
+        letter: "n",
+        x: 2,
+        y: 8
+      }
+    ]
+  },
+  {
+    word: "hp",
+    status: false,
+    tip: "The first of many seeings",
+    tipNumber: 10,
+    color: "#FF0076",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "h",
+        x: 10,
+        y: 8
+      },
+      {
+        letter: "p",
+        x: 9,
+        y: 8
+      }
+    ]
+  },
+  {
+    word: "steve",
+    status: false,
+    tip: "The Mentor",
+    tipNumber: 11,
+    color: "#FFDA89",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "s",
+        x: 10,
+        y: 5
+      },
+      {
+        letter: "t",
+        x: 10,
+        y: 4
+      },
+      {
+        letter: "e",
+        x: 10,
+        y: 3
+      },
+      {
+        letter: "v",
+        x: 10,
+        y: 2
+      },
+      {
+        letter: "e",
+        x: 10,
+        y: 1
+      }
+    ]
+  },
+  {
+    word: "nero",
+    status: false,
+    tip: "Italian Joe",
+    tipNumber: 12,
+    color: "#D5FF89",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "n",
+        x: 8,
+        y: 3
+      },
+      {
+        letter: "e",
+        x: 7,
+        y: 4
+      },
+      {
+        letter: "r",
+        x: 6,
+        y: 5
+      },
+      {
+        letter: "o",
+        x: 5,
+        y: 6
+      }
+    ]
+  },
+  {
+    word: "phd",
+    status: false,
+    tip: "A Docere Philosophus",
+    tipNumber: 13,
+    color: "#89FFD0",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "p",
+        x: 2,
+        y: 4
+      },
+      {
+        letter: "h",
+        x: 3,
+        y: 5
+      },
+      {
+        letter: "d",
+        x: 4,
+        y: 6
+      }
+    ]
+  },
+  {
+    word: "uk",
+    status: false,
+    tip: "The Commonwealth you call Home",
+    tipNumber: 14,
+    color: "#89CEFF",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "u",
+        x: 4,
+        y: 1
+      },
+      {
+        letter: "k",
+        x: 3,
+        y: 2
+      }
+    ]
+  },
+  {
+    word: "us",
+    status: false,
+    tip: "The Home of the (not so) Free",
+    tipNumber: 15,
+    color: "#B089FF",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "u",
+        x: 1,
+        y: 4
+      },
+      {
+        letter: "s",
+        x: 1,
+        y: 5
+      }
+    ]
+  },
+  {
+    word: "sunday",
+    status: false,
+    tip: "Our Routine",
+    tipNumber: 16,
+    color: "#F489FF",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "s",
+        x: 10,
+        y: 6
+      },
+      {
+        letter: "u",
+        x: 9,
+        y: 5
+      },
+      {
+        letter: "n",
+        x: 8,
+        y: 4
+      },
+      {
+        letter: "d",
+        x: 7,
+        y: 3
+      },
+      {
+        letter: "a",
+        x: 6,
+        y: 2
+      },
+      {
+        letter: "y",
+        x: 5,
+        y: 1
+      }
+    ]
+  },
+  {
+    word: "zoom",
+    status: false,
+    tip: "The (slightly defected) companion tool",
+    tipNumber: 17,
+    color: "#FFFF87",
+    textColor: "#111",
+    textTransform: "none",
+    letters: [
+      {
+        letter: "z",
+        x: 9,
+        y: 1
+      },
+      {
+        letter: "o",
+        x: 9,
+        y: 2
+      },
+      {
+        letter: "o",
+        x: 9,
+        y: 3
+      },
+      {
+        letter: "m",
+        x: 9,
+        y: 4
+      }
+    ]
+  },
+  {
+    word: "ides of solmonath",
+    status: false,
+    tip: "Final Answer",
+    tipNumber: 18,
+    color: "#111",
+    textColor: "#fff",
+    textTransform: "bold",
+    letters: [
+      {
+        letter: "i",
+        x: 0,
+        y: 1
+      },
+      {
+        letter: "d",
+        x: 2,
+        y: 1
+      },
+      {
+        letter: "e",
+        x: 7,
+        y: 1
+      },
+      {
+        letter: "s",
+        x: 1,
+        y: 2
+      },
+      {
+        letter: "o",
+        x: 8,
+        y: 2
+      },
+      {
+        letter: "f",
+        x: 1,
+        y: 3
+      },
+      {
+        letter: "s",
+        x: 2,
+        y: 3
+      },
+      {
+        letter: "o",
+        x: 8,
+        y: 5
+      },
+      {
+        letter: "l",
+        x: 2,
+        y: 6
+      },
+      {
+        letter: "m",
+        x: 7,
+        y: 6
+      },
+      {
+        letter: "o",
+        x: 9,
+        y: 6
+      },
+      {
+        letter: "n",
+        x: 1,
+        y: 7
+      },
+      {
+        letter: "a",
+        x: 8,
+        y: 7
+      },
+      {
+        letter: "t",
+        x: 9,
+        y: 7
+      },
+      {
+        letter: "h",
+        x: 10,
+        y: 7
+      }
+    ]
+  }
+];
--- src/pages/DateClue.js
+++ src/pages/DateClue.js
@@ -2,66 +2,189 @@ import Header from "../components/Header";
 import Container from "react-bootstrap/Container";
 import Footer from "../components/Footer";
 import { withFirebase } from "vtr-react-components/dist/Firebase";
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Form, Table } from "react-bootstrap";
+import { DateClueText } from "../components/Communications";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Loader from "../components/Loader";
 
 const DateClueBase = ({ localConfig, changeStage, changeLocalConfig }) => {
   if (localConfig["localStage"] === "decrypt") {
-    return ( <Decryption changeLocalConfig={ changeLocalConfig }/> )
+    return ( <Decryption localConfig={ localConfig }
+                         changeLocalConfig={ changeLocalConfig }
+                         changeStage={ changeStage }/> )
+  } else if (localConfig["localStage"] === "guessed") {
+    return ( <DateClueText changeStage={ changeStage } guessed={true} />)
   } else {
-    return ( <ClueMonologue changeLocalConfig={ changeLocalConfig }/> )
+    return ( <DateClueText changeLocalConfig={ changeLocalConfig } guessed={false} /> )
   }
 };
 
-const Decryption = ({ changeLocalConfig }) => ( <>
-  <Header/>
+const Decryption = ({ localConfig, changeLocalConfig }) => {
+  const [ answer, setAnswer ] = useState("");
+  const [ error, setError ] = useState(false);
+  const [ cantFinish, setCantFinish ] = useState(false);
 
-  <Container style={ { minHeight: "50vh" } }>
-    <h1>DECRYPT THE MESSAGE</h1>
-    <h3 style={ { textIndent: "10%", paddingTop: "5%" } }>
-      TEMP
-    </h3>
-  </Container>
-  <Container style={ { paddingTop: "5%", paddingBottom: "5%", textAlign: "center" } }>
-    <Button variant="danger"
-            onClick={ () => changeLocalConfig("localStage", "reading") }>
-      <h3>Go Back to Prompt</h3>
-    </Button>
-  </Container>
+  const checkAnswer = (event) => {
+    event.preventDefault();
+    setError(false);
+    setCantFinish(false);
+    const words = localConfig["words"];
+    const responseItem = words.find((item) => item["word"].toLowerCase() === answer.toLowerCase());
 
-  <Footer/>
-</> );
+    if (!!responseItem) {
+      if (answer.toUpperCase() === localConfig["finalResponse"].toUpperCase()) {
 
-const ClueMonologue = ({ changeLocalConfig }) => ( <>
-  <Header/>
+        const unfinished = words.find((item) => !item["status"]);
 
-  <Container style={ { minHeight: "50vh" } }>
-    <h1>Agent 1010,</h1>
-    <h3 style={ { textIndent: "10%", paddingTop: "5%" } }>
-      The Secretary has been able to intercept the following message from an unknown associate to Operative 10001:
-    </h3>
-    <h3 style={ { paddingTop: "5%" } }>
-      <strong>10001:</strong> Is it done?
-    </h3>
-    <h3 style={ { paddingTop: "1%" } }>
-      <strong>UNKNOWN:</strong> It’s all as you said…
-    </h3>
-    <h3 style={ { paddingTop: "1%" } }>
-      <strong>10001:</strong> So it’ll be ready in time?
-    </h3>
-    <h3 style={ { paddingTop: "1%" } }>
-      <strong>UNKNOWN:</strong> Yes, on the **** ** *********.
-    </h3>
-  </Container>
-  <Container style={ { paddingTop: "5%", paddingBottom: "5%", textAlign: "center" } }>
-    <Button variant="danger"
-            onClick={ () => changeLocalConfig("localStage", "decrypt") }>
-      <h3>Decrypt</h3>
-    </Button>
-  </Container>
+        if (!!unfinished && unfinished["tipNumber"] !== 18) {
+          console.log(unfinished)
+          setCantFinish(true);
+        } else {
+          const index = words.indexOf(responseItem);
+          responseItem["status"] = true;
+          words[index] = responseItem;
 
-  <Footer/>
-</> );
+          changeLocalConfig("words", words);
+          changeLocalConfig("localStage", "guessed")
+        }
+      } else {
+        const index = words.indexOf(responseItem);
+        responseItem["status"] = true;
+        words[index] = responseItem;
+
+        changeLocalConfig("words", words);
+      }
+    } else {
+      setError(true);
+    }
+  }
+
+  return ( <>
+    <Header/>
+
+    <Container style={ { minHeight: "50vh" } }>
+      <h1>DECRYPT THE MESSAGE</h1>
+      <h3 style={ { paddingTop: "3%" } }>If all the words you can find, then in the remainder you will find your
+        key...</h3>
+
+      { error && <h5 style={ { paddingTop: "3%" } }> Incorrect answer! </h5> }
+      { cantFinish && <h5 style={ { paddingTop: "3%" } }> Nice try! But you need to guess them all :) </h5> }
+
+      <GameBoard localConfig={ localConfig }/>
+      <Row>
+        <Form onSubmit={ checkAnswer }>
+          <Row>
+            <Col>
+              <h3 style={ { paddingBottom: "5%" } }>Enter Answer:</h3>
+            </Col>
+            <Col sm={ 8 }>
+              <Form.Group style={ { paddingBottom: "3%" } }
+                          className="mb-3"
+                          controlId="formBasicAnswer">
+                <Form.Control type="text"
+                              placeholder="Answer..."
+                              defaultValue={ answer }
+                              onChange={ (event) => setAnswer(event.target.value) }/>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row style={ { paddingTop: "5%", paddingBottom: "5%" } }>
+            <Col>
+              <Button variant="danger"
+                      type="submit">
+                <h3>Check Answer</h3>
+              </Button>
+            </Col>
+            <Col>
+              <Button variant="danger"
+                      onClick={ () => changeLocalConfig("localStage", "reading") }>
+                <h3>Go Back to Prompt</h3>
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Row>
+    </Container>
+
+    <Footer/>
+  </> );
+}
+
+const GameBoard = ({ localConfig }) => {
+  const [ board ] = useState([ ...Array(9) ].map(x => Array(12).fill({})));
+  const [ tips ] = useState([ [], [] ])
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    localConfig["words"].forEach((word) => {
+      word["letters"].forEach((letter) => {
+        board[letter["y"]][letter["x"]] = {
+          status: word["status"],
+          color: word["color"],
+          textColor: word["textColor"],
+          textTransform: word["textTransform"],
+          letter: letter["letter"].toUpperCase()
+        };
+      });
+    });
+
+    localConfig["words"].forEach((word) => {
+      const index = ( ( word["tipNumber"] % 2 ) + 1 ) % 2;
+      tips[index].push({
+                         tipNumber: word["tipNumber"],
+                         tip: word["tip"],
+                         status: word["status"]
+                       });
+    });
+    setLoading(false)
+  }, [ board, tips, localConfig ]);
+
+  if (loading) {
+    return ( <Loader opacity={ 100 }/> )
+  }
+
+  return (
+    <Row style={ { paddingBottom: "5%", paddingTop: "5%" } }>
+      <Col>
+        <Table bordered
+               style={ { textAlign: "center" } }>
+          <tbody>
+          { board.map((rowEntry, index) => <TableRow key={ index }
+                                                     rowData={ rowEntry }/>) }
+          </tbody>
+        </Table>
+      </Col>
+      { tips.map((colData, index) => <TipCol colData={ colData }
+                                             key={ index }/>) }
+    </Row>
+  );
+};
+
+const TipCol = ({ colData }) => (
+  <Col>
+    { colData.map((entryData, index) => (
+      <p key={ index }
+         style={ { textDecoration: entryData["status"] ? "line-through" : "none" } }><strong>Tip
+        # { entryData["tipNumber"] }</strong> { entryData["tip"] }</p> )) }
+  </Col>
+);
+
+const TableRow = ({ rowData }) => (
+  <tr>
+    { rowData.map((entryData, index) => <TableEntry key={ index }
+                                                    entryData={ entryData }/>) }
+  </tr>
+);
+
+const TableEntry = ({ entryData }) => (
+  <th style={ {
+    background: entryData["status"] ? entryData["color"] : "#fff",
+    color: entryData["status"] ? entryData["textColor"] : "#111",
+    textTransform: entryData["status"] ? entryData["textTransform"] : "none"
+  } }>{ entryData["letter"] }</th>
+);
 
 
 const DateClue = withFirebase(DateClueBase);
